f2b4a293fd0d014eeb93b10101eb1717004a661a
Aditya Sharma <adityasharma96655@gmail.com>
2025-04-15 12:18:29 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const router = createRouter({
           component: () => import('../views/MyStrategies.vue'),
         },
         {
-          path: '/dashboard',
+          path: '/',
           name: 'dashboard',
           component: () => import('@/views/Dashboard.vue'),
         },
